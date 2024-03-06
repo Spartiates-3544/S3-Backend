f9fe081d06c4b0b747fa730efe942bdb0a86aa29
Charles200-beep <60555764+Charles200-beep@users.noreply.github.com>
2024-03-05 23:17:10 -0500

    Added test data, support for multiple jsons in an array, updated excel generation

--- backend.py
+++ backend.py
@@ -10,50 +10,103 @@ MATCH_DIRECTORY = 'matches'
 OUTPUT_DIRECTORY = 'output'
 
 def scanAndDecode():
-    if not os.path.isdir('matches'):
+    if not os.path.isdir(MATCH_DIRECTORY):
         os.mkdir(MATCH_DIRECTORY)
 
     response = str(qrcode.detect())
     responseJson = response.strip()
     responseJson = json.loads(responseJson)
-    
-    with open('{}/{}{}.json'.format(MATCH_DIRECTORY, responseJson['general']['matchType']['value'], responseJson['general']['matchNumber']['value']), 'w') as file:
-        file.write(response)
+        
+    for match in responseJson:
+        path = os.path.join(MATCH_DIRECTORY, '{}{}_{}.json'.format(match['General']['matchType'], match['General']['matchNumber'], match['General']['teamNb']))   
+        with open(path, 'w') as file:
+            file.write(json.dumps(match))
 
+def scanAndDecode(image):
+    if not os.path.isdir(MATCH_DIRECTORY):
+        os.mkdir(MATCH_DIRECTORY)
+
+    response = str(qrcode.detect(image))
+    responseJson = response.strip()
+    responseJson = json.loads(responseJson)
+    
+    if isinstance(responseJson, list):
+        for match in responseJson:
+            path = os.path.join(MATCH_DIRECTORY, '{}{}_{}.json'.format(match['General']['matchType'], match['General']['matchNumber'], match['General']['teamNb']))   
+            with open(path, 'w') as file:
+                file.write(json.dumps(match))
+    else:
+        path = os.path.join(MATCH_DIRECTORY, '{}{}_{}.json'.format(responseJson['General']['matchType'], responseJson['General']['matchNumber'], responseJson['General']['teamNb']))   
+        with open(path, 'w') as file:
+            file.write(json.dumps(responseJson))
+
+#This is what has to be changed every year
 def generateExcel():
     # try:
         timestamp = datetime.strftime(datetime.now(), '%Y-%m-%dT%H%M')
-        table = pd.DataFrame()
+        rawDataTable = pd.DataFrame()
 
-        rawDataSheet = 'rawData'
+        rawDataSheetName = 'rawData'
+        averagesDataSheetName = 'Team Averages'
 
-        writer = pd.ExcelWriter('{directory:}/{filename:}.xlsx'.format(directory = OUTPUT_DIRECTORY, filename = timestamp), engine  = 'xlsxwriter')
-
-        for filename in os.listdir(MATCH_DIRECTORY):
-            with open(os.path.join(MATCH_DIRECTORY, filename)) as file:
-                matchJson = json.load(file)
-                data = pd.json_normalize(matchJson)
-                table = pd.concat([table, data])
+        teamList = []
 
         if not os.path.isdir(OUTPUT_DIRECTORY):
             try:
                 os.mkdir(OUTPUT_DIRECTORY)
             except FileExistsError as e:
                 print(e)
-            # else:
-            #     table.to_excel('{directory:}/{filename:}.xlsx'.format(directory = OUTPUT_DIRECTORY, filename = timestamp), index=False)
+            else:
+                writer = pd.ExcelWriter('{directory:}/{filename:}.xlsx'.format(directory = OUTPUT_DIRECTORY, filename = timestamp), engine='xlsxwriter')
         else:
-            table.to_excel(writer, index=False, sheet_name = rawDataSheet)
+            writer = pd.ExcelWriter('{directory:}/{filename:}.xlsx'.format(directory = OUTPUT_DIRECTORY, filename = timestamp), engine='xlsxwriter')
 
-        # adjust the column widths based on the content
-        worksheet = writer.sheets[rawDataSheet]    
+        #Create table of all data in matches directory
+        for filename in os.listdir(MATCH_DIRECTORY):
+            with open(os.path.join(MATCH_DIRECTORY, filename)) as file:
+                matchJson = json.load(file)
 
-        for i, col in enumerate(table.columns):
-            width = max(table[col].apply(lambda x: len(str(x))).max(), len(col))
-            worksheet.set_column(i, i, width)
+                #Create list of teams
+                team = matchJson['General']['teamNb']
+                if not team in teamList:
+                    teamList.append(team)
+
+                data = pd.json_normalize(matchJson)
+                rawDataTable = pd.concat([rawDataTable, data])
+
+        #Export to excel
+        rawDataTable.to_excel(writer, index=False, sheet_name = rawDataSheetName)
+
+        #Adjust the column widths based on the content
+        rawDataWorksheet = writer.sheets[rawDataSheetName]    
+        for i, col in enumerate(rawDataTable.columns):
+            width = max(rawDataTable[col].apply(lambda x: len(str(x))).max(), len(col))
+            rawDataWorksheet.set_column(i, i, width)
+        
+        #Calculate point averages for every team and make DataFrame
+        averagesDataTable = pd.DataFrame()
+
+        for team in teamList:
+            teamRows = rawDataTable.loc[rawDataTable['General.teamNb'] == team]
+            averagesDict = {
+                'ampAutonAverage' : teamRows['Autonome.ampPts'].mean(), 'speakerAutonAverage' : teamRows['Autonome.speakerPts'].mean(),
+                'ampTeleopAverage' : teamRows['Tele-Op.ampPts'].mean(), 'speakerTeleopAverage' : teamRows['Tele-Op.speakerPts'].mean()
+                }
+            table = pd.DataFrame(data=averagesDict, index=[team])
+            averagesDataTable = pd.concat([averagesDataTable, table])
+
+        #Export to excel
+        averagesDataTable.to_excel(writer, index=True, sheet_name=averagesDataSheetName)
+
+        #Adjust the column width based on the content
+        averagesWorksheet = writer.sheets[averagesDataSheetName]
+        for i, col in enumerate(averagesDataTable.columns):
+            width = max(averagesDataTable[col].apply(lambda x: len(str(x))).max(), len(col))
+            averagesWorksheet.set_column(i, i, width)
 
         writer.close()
     # except:
     #     raise Exception('Error generating excel file! Check console output.')
             
 generateExcel()
+# scanAndDecode()
--- qrcode.py
+++ qrcode.py
@@ -4,12 +4,26 @@ from qreader import QReader
 cap = cv2.VideoCapture(0)
 qreader = QReader()
 
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+#if not cap.isOpened():
+#    raise IOError("Cannot open webcam")
 
 def detect():
     while True:
         _, img = cap.read()
+        data = qreader.detect_and_decode(image=img, return_detections=False)
+        cv2.imshow('QR Code Scanner', img)
+        cv2.waitKey(1)
+        if data and data != (None,):
+            break
+
+    dataStr = data[0]
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    return dataStr
+
+def detect(img):
+    while True:
+        # _, img = cap.read()
         data = qreader.detect_and_decode(image=img, return_detections=False)
         cv2.imshow('QR Code Scanner', img)
         cv2.waitKey(1)
